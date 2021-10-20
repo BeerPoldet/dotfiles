59a38991db774d53320481a1feefbe110aebc762
beerpoldet <poladet.a@gmail.com>
2021-10-20 14:38:50 +0700

    Fix incorrect path for nvim

--- symlink.sh
+++ symlink.sh
@@ -1,6 +1,6 @@
 git clone git@github.com:beerpoldet/dotfiles.git ~/dev/dotfiles
 
-ln -s ~/dev/dotfiles/dev/dotfiles/nvim ~/.config/nvim
+ln -s ~/dev/dotfiles/nvim ~/.config
 ln -s ~/dev/dotfiles/alacritty.yml ~/.config/alacritty/alacritty.yml
 ln -s ~/dev/dotfiles/.gitconfig ~/.gitconfig
 ln -s ~/dev/dotfiles/.my.conf ~/.my.conf
