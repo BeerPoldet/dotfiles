7d06f8db85144549bfed6ba9cbebd6a7f97593fb
beerpoldet <poladet.a@gmail.com>
2023-02-28 22:40:33 +0700

    Start using lazy vim

--- nvim/.config/nvim/after/plugin/tree.rc.lua
+++
@@ -1,7 +0,0 @@
-local status, nvim_tree = pcall(require, "nvim-tree")
-if (not status) then return end
-
-nvim_tree.setup()
-
-vim.keymap.set('n', '<C-n>', '<Cmd>NvimTreeToggle<CR>', { silent = true })
-vim.keymap.set('n', '<C-f>', '<Cmd>NvimTreeFocus<CR>', { silent = true })
